0488a1b259e0d1855b64cbace9e910a596c2b191
hanzala226 <166871122+hanzala226@users.noreply.github.com>
2024-05-17 13:55:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function chng1(){
+    document.getElementById("ch1").src="5.jpg"
+}
+function chng2(){
+    document.getElementById("ch2").src="5.jpg"
+}
+function chng3(){
+    document.getElementById("ch3").src="5.jpg"
+}
+function chng4(){
+    document.getElementById("ch4").src="5.jpg"
+}
+function reg(){
+
+    if(
+        document.getElementsByTagName("input").value
+    )
+    alert("Succesfull")
+}
